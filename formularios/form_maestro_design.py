6ade922c9007b111049f207cd4033fd421412878
Anthony Silva <anthony.silvac@gmail.com>
2024-07-07 18:13:44 -0400

    se agrega menu lateral

--- formularios/form_maestro_design.py
+++ formularios/form_maestro_design.py
@@ -11,6 +11,7 @@ class FormularioMaestroDesign(tk.Tk):
         self.config_window()
         self.paneles()
         self.controles_barra_superior()
+        self.controles_menu_lateral()
         
 
     def config_window(self):
@@ -31,14 +32,50 @@ class FormularioMaestroDesign(tk.Tk):
         self.cuerpo_principal.pack(side=tk.RIGHT, fill='both', expand=True)
     
     def controles_barra_superior(self):
-        font_awasome = font.Font(family='FontAwesome', size=12)
+        font_awesome = font.Font(family='FontAwesome', size=12)
 
-        self.labelTitulo = tk.Label(self.barra_superior, text="Anthony Silva")
-        self.labelTitulo.config(fg="#fff", font=("Roboto", 15), bg=COLOR_BARRA_SUPERIOR, pady=10, width=16)
+        self.buttonMenuLateral = tk.Button(self.barra_superior, text="\uf0c9", font=font_awesome, bd=0, bg=COLOR_BARRA_SUPERIOR, fg="white" )
+        self.buttonMenuLateral.pack(side=tk.LEFT)
+
+        self.labelTitulo = tk.Label(self.barra_superior, text="CENTRO MULTIMEDIA DE ENTRETENIMIENTO")
+        self.labelTitulo.config(fg="#fff", font=("Roboto", 15), bg=COLOR_BARRA_SUPERIOR, pady=10, width=100)
         self.labelTitulo.pack(side=tk.LEFT)
 
-        self.buttonMenuLateral = tk.Button(self.barra_superior, text="\uf0c9", font=font_awasome, bd=0, bg=COLOR_BARRA_SUPERIOR, fg="white" )
-        self.buttonMenuLateral.pack(side=tk.LEFT)
+        
+
+    def controles_menu_lateral(self):
+        ancho_menu = 20
+        alto_menu = 2
+        font_awesome = font.Font(family='FontAwesome', size=12)
+
+        self.buttonPeliculas = tk.Button(self.menu_lateral, text="Películas", font=font_awesome, fg="white", bd=0, bg=COLOR_MENU_LATERAL, width=ancho_menu, height=alto_menu )
+        self.buttonPeliculas.pack(side=tk.TOP)
+        self.configurar_boton_menu(self.buttonPeliculas)
+
+        self.buttonSeries = tk.Button(self.menu_lateral, text="Series", font=font_awesome, fg="white", bd=0, bg=COLOR_MENU_LATERAL, width=ancho_menu, height=alto_menu  )
+        self.buttonSeries.pack(side=tk.TOP)
+        self.configurar_boton_menu(self.buttonSeries)
+
+        self.buttonAnime = tk.Button(self.menu_lateral, text="Anime", font=font_awesome, fg="white", bd=0, bg=COLOR_MENU_LATERAL, width=ancho_menu, height=alto_menu  )
+        self.buttonAnime.pack(side=tk.TOP)
+        self.configurar_boton_menu(self.buttonAnime)
+
+        self.buttonConfiguracion= tk.Button(self.menu_lateral, text="Configuracion", font=font_awesome, fg="white", bd=0, bg=COLOR_MENU_LATERAL, width=ancho_menu, height=alto_menu  )
+        self.buttonConfiguracion.pack(side=tk.TOP)
+        self.configurar_boton_menu(self.buttonConfiguracion)
+
+    def configurar_boton_menu(self, button):
+        self.bind_hover_events(button)
+
+    def bind_hover_events(self, button):
+        button.bind("<Enter>", lambda event: self.on_enter(event, button))
+        button.bind("<Leave>", lambda event: self.on_leave(event, button))
+    
+    def on_enter(self, event, button):
+        button.config(bg=COLOR_MENU_HOVER, fg='white')
+
+    def on_leave(self, event, button):
+        button.config(bg=COLOR_MENU_LATERAL, fg='white')
 
 
 
